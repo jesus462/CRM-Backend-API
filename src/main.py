96b5b5ec336ba7ed309ed756917683891e5d300d
jesus462 <jesusponce.462@gmail.com>
2020-02-24 17:01:03 +0000

    Client back

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+import json
+from flask import Flask, request, jsonify, url_for, make_response
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Client
 #from models import Person
 
 app = Flask(__name__)
@@ -37,7 +38,48 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/clients', methods=['POST', 'GET'])
+@app.route('/clients/<client_id>', methods=['DELETE', 'PUT'])
+def handle_clients(client_id=0):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    if request.method == "POST":
+        print("creating client")
+        client = request.json
+        print(request.json)
+        new_client = Client(client["name"], client["lastName"], client["company"], client["position"], client["email"], client["phone"], client["extraPhone"], client["sector"], client["city"], client["country"], client["linkedin"], client["source"], client["observations"])
+        db.session.add(new_client)
+        db.session.commit()
+        response_body = {
+            "status": "HTTP_200_OK. Ok"
+        }
+        status_code = 200
+
+    elif request.method == "GET":
+        user_client_list = Client.query.all()
+        response_body = []
+        for client in user_client_list:
+            response_body.append(client.serialize())
+        status_code = 200
+
+    elif request.method == "DELETE":
+        Client.query.filter_by(id=client_id).delete()
+        db.session.commit()
+        response_body = {
+            "result": "ok",
+            "status": "HTTP_204_NO_CONTENT. User and tasks deleted."
+        }
+        status_code = 204
+    
+    return make_response(
+        json.dumps(response_body),
+        status_code,
+        headers
+    )
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
--- src/models.py
+++ src/models.py
@@ -2,6 +2,56 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Client(db.Model):
+    """ clients of the funnel """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    company = db.Column(db.String(50))
+    position = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    phone = db.Column(db.String(50))
+    extra_phone = db.Column(db.String(50))
+    sector = db.Column(db.String(50))
+    city = db.Column(db.String(50))
+    country = db.Column(db.String(50))
+    linkedin = db.Column(db.String(50))
+    source = db.Column(db.String(50))
+    observations = db.Column(db.String(400))
+
+    def __init__(self, name, lastName, company, position, email, phone, extraPhone, sector, city, country, linkedin, source, observations):
+        self.name = name
+        self.last_name = lastName
+        self.company = company
+        self.position = position
+        self.email = email
+        self.phone = phone
+        self.extra_phone = extraPhone
+        self.sector = sector
+        self.city = city
+        self.country = country
+        self.linkedin = linkedin
+        self.source = source
+        self.observations = observations
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastName": self.last_name,
+            "company": self.company,
+            "position": self.position,
+            "email": self.email,
+            "phone": self.phone,
+            "extraPhone": self.extra_phone,
+            "sector": self.sector,
+            "city": self.city,
+            "country": self.country,
+            "linkedin": self.linkedin,
+            "source": self.source,
+            "observations": self.observations
+        }
+
 # class Person(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     username = db.Column(db.String(80), unique=True, nullable=False)
